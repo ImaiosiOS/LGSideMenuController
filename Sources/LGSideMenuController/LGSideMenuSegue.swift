8a8941b6b7f007db378c30a094f4cf47ba6be7b0
ImaiosiOS <imaios.ios@imaios.com>
2026-07-01 15:05:04 +0200

    Update LGSideMenuSegue.swift

--- Sources/LGSideMenuController/LGSideMenuSegue.swift
+++ Sources/LGSideMenuController/LGSideMenuSegue.swift
@@ -39,11 +39,10 @@ public final class LGSideMenuSegue: UIStoryboardSegue {
     }
 
     public override func perform() {
-        guard self.source is LGSideMenuController else {
+        guard let sideMenuController = self.source as? LGSideMenuController else { 
             assert(false, "LGSideMenuSegue must have source as LGSideMenuController") 
-            return
+            return 
         }
-        let sideMenuController = self.source as! LGSideMenuController
         switch identifier {
         case Identifier.root:
             sideMenuController.rootViewController = destination
